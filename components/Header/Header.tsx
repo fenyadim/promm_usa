d80b7e72a9e799bb106480ed00dbdb5d311f3d77
fenyadim <dimos9813@gmail.com>
2023-06-19 08:51:15 +0800

    add

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,170 +1,178 @@
-'use client';
+'use client'
 
-import { Dispatch, FC, SetStateAction, useEffect, useRef, useState } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import cn from 'classnames';
+import { FC, useEffect, useRef, useState } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
-import styles from './Header.module.scss';
-import ImageBtn from '../ImageBtn/ImageBtn';
-import Menu from '../Menu/Menu';
-import { toggleVisible } from '@/utils/toggleVisible';
+import styles from 'Header.module.scss'
+import ImageBtn from '@/components/ImageBtn/ImageBtn'
+
+import { toggleVisible } from '@/utils/toggleVisible'
+import Menu from '@/components/Menu/Menu'
 
 const Header: FC = () => {
-  const [showMenu, setShowMenu] = useState<boolean>(false);
-  const [showSearch, setShowSearch] = useState<boolean>(false);
-  const [showMobileMenu, setShowMobileMenu] = useState<boolean>(false);
-  const [innerWidth, setInnerWidth] = useState<number>(0);
-  const searchRef = useRef<any>(null);
-  const btnSearchRef = useRef<any>(null);
+	const [showMenu, setShowMenu] = useState<boolean>(false)
+	const [showSearch, setShowSearch] = useState<boolean>(false)
+	const [showMobileMenu, setShowMobileMenu] = useState<boolean>(false)
+	const [innerWidth, setInnerWidth] = useState<number>(0)
+	const searchRef = useRef<any>(null)
+	const btnSearchRef = useRef<any>(null)
 
-  const topLinks = [
-    { slug: 'payment', title: 'Payment and delivery' },
-    { slug: 'blog', title: 'Blog' },
-    { slug: 'company', title: 'Company' },
-    { slug: 'help', title: 'Help 24/7' },
-    { slug: 'media-about', title: 'The media about Us' },
-    { slug: 'contact', title: 'Contact' },
-  ];
+	const topLinks = [
+		{ slug: 'payment', title: 'Payment and delivery' },
+		{ slug: 'blog', title: 'Blog' },
+		{ slug: 'company', title: 'Company' },
+		{ slug: 'help', title: 'Help 24/7' },
+		{ slug: 'media-about', title: 'The media about Us' },
+		{ slug: 'contact', title: 'Contact' },
+	]
 
-  useEffect(() => {
-    const onClick = (e: Event) => {
-      if (
-        btnSearchRef.current &&
-        !btnSearchRef.current.contains(e.target) &&
-        searchRef.current &&
-        !searchRef.current.contains(e.target)
-      ) {
-        setShowSearch(false);
-      }
-    };
+	useEffect(() => {
+		const onClick = (e: Event) => {
+			if (
+				btnSearchRef.current &&
+				!btnSearchRef.current.contains(e.target) &&
+				searchRef.current &&
+				!searchRef.current.contains(e.target)
+			) {
+				setShowSearch(false)
+			}
+		}
 
-    document.addEventListener('click', onClick);
-    return () => document.removeEventListener('click', onClick);
-  }, [searchRef, btnSearchRef]);
+		document.addEventListener('click', onClick)
+		return () => document.removeEventListener('click', onClick)
+	}, [searchRef, btnSearchRef])
 
-  useEffect(() => {
-    if (innerWidth === 0) {
-      setInnerWidth(window.innerWidth);
-    }
-    const resizeWindos = (e: Event) => {
-      const target = e.target as Window;
-      setInnerWidth(target.innerWidth);
-    };
-    window.addEventListener('resize', resizeWindos);
-    return () => {
-      window.removeEventListener('resize', resizeWindos);
-    };
-  }, []);
+	useEffect(() => {
+		if (innerWidth === 0) {
+			setInnerWidth(window.innerWidth)
+		}
+		const resizeWindos = (e: Event) => {
+			const target = e.target as Window
+			setInnerWidth(target.innerWidth)
+		}
+		window.addEventListener('resize', resizeWindos)
+		return () => {
+			window.removeEventListener('resize', resizeWindos)
+		}
+	}, [])
 
-  const toggleMobileMenu = toggleVisible(setShowMobileMenu, showMobileMenu);
+	const toggleMobileMenu = toggleVisible(setShowMobileMenu, showMobileMenu)
 
-  const toggleMenu = toggleVisible(setShowMenu, showMenu);
+	const toggleMenu = toggleVisible(setShowMenu, showMenu)
 
-  const toggleSearch = toggleVisible(setShowSearch, showSearch);
+	const toggleSearch = toggleVisible(setShowSearch, showSearch)
 
-  return (
-    <>
-      <header className={styles.header}>
-        <nav className={styles.nav_top}>
-          <ul className={styles.nav_top_left}>
-            {topLinks.map(({ slug, title }) => (
-              <li key={slug}>
-                <Link href="#">{title}</Link>
-              </li>
-            ))}
-          </ul>
-          <p>5261 W Imperial Hwy, Los Angeles, CA 90045, USA</p>
-        </nav>
-        <input
-          ref={searchRef}
-          type="text"
-          className={styles.header_input}
-          style={showSearch ? { display: 'block', width: '100%' } : { display: 'none' }}
-        />
-        <div
-          className={styles.header_middle}
-          style={!showSearch ? { display: 'grid' } : { display: 'none' }}>
-          <Link href="#" className={styles.logo_link}>
-            <Image
-              src="/image/header/logo.png"
-              alt="Promminer"
-              fill
-              style={{ objectFit: 'contain' }}
-            />
-          </Link>
-          <div className={styles.dropdown}>
-            <button onClick={toggleMenu} className={styles.catalog_btn}>
-              <Image
-                src="/image/header/burger-menu-svgrepo-com (2).svg"
-                alt="catalog"
-                width={16}
-                height={16}
-              />
-              Catalog
-            </button>
-          </div>
-          {innerWidth < 860 ? (
-            innerWidth < 570 ? (
-              <>
-                <ImageBtn
-                  ref={btnSearchRef}
-                  srcImg="/image/header/search_black.svg"
-                  onClick={toggleSearch}
-                />
-                <ImageBtn
-                  onClick={toggleMobileMenu}
-                  srcImg="/image/header/burger-menu-svgrepo-com (1).svg"
-                />
-              </>
-            ) : (
-              <ImageBtn
-                ref={btnSearchRef}
-                title="Search"
-                srcImg="/image/header/search_black.svg"
-                onClick={toggleSearch}
-              />
-            )
-          ) : (
-            <input type="text" className={styles.header_input} />
-          )}
-          <div className={styles.header_right}>
-            <ImageBtn title="Basket" srcImg="/image/header/basket_icon_160414.svg" />
-            {innerWidth > 860 && (
-              <>
-                <ImageBtn
-                  title="Favorites"
-                  srcImg="/image/header/star-favorite-1498-svgrepo-com.svg"
-                />
-                <ImageBtn
-                  title="Order a call"
-                  srcImg="/image/header/auricular-phone-symbol-in-a-circle_icon-icons.com_56570.svg"
-                />
-              </>
-            )}
-          </div>
-        </div>
+	return (
+		<>
+			<header className={styles.header}>
+				<nav className={styles.nav_top}>
+					<ul className={styles.nav_top_left}>
+						{topLinks.map(({ slug, title }) => (
+							<li key={slug}>
+								<Link href="#">{title}</Link>
+							</li>
+						))}
+					</ul>
+					<p>5261 W Imperial Hwy, Los Angeles, CA 90045, USA</p>
+				</nav>
+				<input
+					ref={searchRef}
+					type="text"
+					className={styles.header_input}
+					style={
+						showSearch
+							? { display: 'block', width: '100%' }
+							: { display: 'none' }
+					}
+				/>
+				<div
+					className={styles.header_middle}
+					style={!showSearch ? { display: 'grid' } : { display: 'none' }}
+				>
+					<Link href="#" className={styles.logo_link}>
+						<Image
+							src="/image/header/logo.png"
+							alt="Promminer"
+							fill
+							style={{ objectFit: 'contain' }}
+						/>
+					</Link>
+					<div className={styles.dropdown}>
+						<button onClick={toggleMenu} className={styles.catalog_btn}>
+							<Image
+								src="/image/header/burger-menu-svgrepo-com (2).svg"
+								alt="catalog"
+								width={16}
+								height={16}
+							/>
+							Catalog
+						</button>
+					</div>
+					{innerWidth < 860 ? (
+						innerWidth < 570 ? (
+							<>
+								<ImageBtn
+									ref={btnSearchRef}
+									srcImg="/image/header/search_black.svg"
+									onClick={toggleSearch}
+								/>
+								<ImageBtn
+									onClick={toggleMobileMenu}
+									srcImg="/image/header/burger-menu-svgrepo-com (1).svg"
+								/>
+							</>
+						) : (
+							<ImageBtn
+								ref={btnSearchRef}
+								title="Search"
+								srcImg="/image/header/search_black.svg"
+								onClick={toggleSearch}
+							/>
+						)
+					) : (
+						<input type="text" className={styles.header_input} />
+					)}
+					<div className={styles.header_right}>
+						<ImageBtn
+							title="Basket"
+							srcImg="/image/header/basket_icon_160414.svg"
+						/>
+						{innerWidth > 860 && (
+							<>
+								<ImageBtn
+									title="Favorites"
+									srcImg="/image/header/star-favorite-1498-svgrepo-com.svg"
+								/>
+								<ImageBtn
+									title="Order a call"
+									srcImg="/image/header/auricular-phone-symbol-in-a-circle_icon-icons.com_56570.svg"
+								/>
+							</>
+						)}
+					</div>
+				</div>
 
-        <nav className={styles.nav_bot}>
-          <ul>
-            <li>
-              <Link href="">Investing in mining</Link>
-            </li>
-            <li>
-              <Link href="">Turnkey Mining</Link>
-            </li>
-            <li>
-              <Link href="">Data Center</Link>
-            </li>
-            <li>
-              <Link href="">Equipment leasing</Link>
-            </li>
-            <li>
-              <Link href="">Ready-made business</Link>
-            </li>
-          </ul>
-        </nav>
-        {/* <div
+				<nav className={styles.nav_bot}>
+					<ul>
+						<li>
+							<Link href="">Investing in mining</Link>
+						</li>
+						<li>
+							<Link href="">Turnkey Mining</Link>
+						</li>
+						<li>
+							<Link href="">Data Center</Link>
+						</li>
+						<li>
+							<Link href="">Equipment leasing</Link>
+						</li>
+						<li>
+							<Link href="">Ready-made business</Link>
+						</li>
+					</ul>
+				</nav>
+				{/* <div
           className={styles.dropdown_container}
           style={showMenu ? { opacity: 1 } : { opacity: 0, zIndex: -1 }}>
           <div>
@@ -197,66 +205,72 @@ const Header: FC = () => {
             <Link href="#">Ready-made business-STANDART</Link>
           </div>
         </div> */}
-      </header>
-      <Menu showMenu={showMenu} toggleMenu={toggleMenu} zIndex={2}>
-        <div>
-          <Link href="#">Asic Miner</Link>
-          <Link href="#">Bitmain</Link>
-          <Link href="#">Whatsminer</Link>
-          <Link href="#">Innosilicon</Link>
-          <Link href="#">Canaan</Link>
-          <Link href="#">Goldshell</Link>
-          <Link href="#">iPollo</Link>
-        </div>
-        <div>
-          <Link href="#">Mining containers</Link>
-          <Link href="#">PM Small Container</Link>
-          <Link href="#">PM Start container</Link>
-          <Link href="#">PM Medium container</Link>
-          <Link href="#">PM Optimal container</Link>
-          <Link href="#">PM Big container</Link>
-        </div>
-        <div>
-          <Link href="#">Ready-made business</Link>
-          <Link href="#">Ready-made business-START</Link>
-          <Link href="#">Ready-made business-SMART</Link>
-          <Link href="#">Ready-made business-MEDIUM</Link>
-          <Link href="#">Ready-made business-BIG</Link>
-          <Link href="#">Ready-made business-MAX</Link>
-          <Link href="#">Ready-made business-VIP</Link>
-          <Link href="#">Ready-made business-PREMIUM</Link>
-          <Link href="#">Ready-made business-TO THE MOON</Link>
-          <Link href="#">Ready-made business-STANDART</Link>
-        </div>
-      </Menu>
-      <Menu showMenu={showMobileMenu} toggleMenu={toggleMobileMenu}>
-        <Link href="">Investing in mining</Link>
-        <Link href="">Turnkey Mining</Link>
-        <Link href="">Data Center</Link>
-        <Link href="">Equipment leasing</Link>
-        <Link href="">Ready-made business</Link>
-        {topLinks.map(({ slug, title }) => (
-          <Link key={slug} href="#">
-            {title}
-          </Link>
-        ))}
-      </Menu>
-      <div className={styles.bot_menu}>
-        <ImageBtn
-          srcImg="/image/header/catalog.svg"
-          title="Catalog"
-          sizeImg={30}
-          onClick={toggleMenu}
-        />
-        <ImageBtn title="Basket" srcImg="/image/header/basket_icon_160414.svg" />
-        <ImageBtn title="Favorites" srcImg="/image/header/star-favorite-1498-svgrepo-com.svg" />
-        <ImageBtn
-          title="Order a call"
-          srcImg="/image/header/auricular-phone-symbol-in-a-circle_icon-icons.com_56570.svg"
-        />
-      </div>
-    </>
-  );
-};
+			</header>
+			<Menu showMenu={showMenu} toggleMenu={toggleMenu} zIndex={2}>
+				<div>
+					<Link href="#">Asic Miner</Link>
+					<Link href="#">Bitmain</Link>
+					<Link href="#">Whatsminer</Link>
+					<Link href="#">Innosilicon</Link>
+					<Link href="#">Canaan</Link>
+					<Link href="#">Goldshell</Link>
+					<Link href="#">iPollo</Link>
+				</div>
+				<div>
+					<Link href="#">Mining containers</Link>
+					<Link href="#">PM Small Container</Link>
+					<Link href="#">PM Start container</Link>
+					<Link href="#">PM Medium container</Link>
+					<Link href="#">PM Optimal container</Link>
+					<Link href="#">PM Big container</Link>
+				</div>
+				<div>
+					<Link href="#">Ready-made business</Link>
+					<Link href="#">Ready-made business-START</Link>
+					<Link href="#">Ready-made business-SMART</Link>
+					<Link href="#">Ready-made business-MEDIUM</Link>
+					<Link href="#">Ready-made business-BIG</Link>
+					<Link href="#">Ready-made business-MAX</Link>
+					<Link href="#">Ready-made business-VIP</Link>
+					<Link href="#">Ready-made business-PREMIUM</Link>
+					<Link href="#">Ready-made business-TO THE MOON</Link>
+					<Link href="#">Ready-made business-STANDART</Link>
+				</div>
+			</Menu>
+			<Menu showMenu={showMobileMenu} toggleMenu={toggleMobileMenu}>
+				<Link href="">Investing in mining</Link>
+				<Link href="">Turnkey Mining</Link>
+				<Link href="">Data Center</Link>
+				<Link href="">Equipment leasing</Link>
+				<Link href="">Ready-made business</Link>
+				{topLinks.map(({ slug, title }) => (
+					<Link key={slug} href="#">
+						{title}
+					</Link>
+				))}
+			</Menu>
+			<div className={styles.bot_menu}>
+				<ImageBtn
+					srcImg="/image/header/catalog.svg"
+					title="Catalog"
+					sizeImg={30}
+					onClick={toggleMenu}
+				/>
+				<ImageBtn
+					title="Basket"
+					srcImg="/image/header/basket_icon_160414.svg"
+				/>
+				<ImageBtn
+					title="Favorites"
+					srcImg="/image/header/star-favorite-1498-svgrepo-com.svg"
+				/>
+				<ImageBtn
+					title="Order a call"
+					srcImg="/image/header/auricular-phone-symbol-in-a-circle_icon-icons.com_56570.svg"
+				/>
+			</div>
+		</>
+	)
+}
 
-export default Header;
+export default Header
--- pages/Home/Home.tsx
+++ pages/Home/Home.tsx
@@ -1,172 +1,181 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import { FC } from 'react';
-import Slider from 'react-slick';
+import Image from 'next/image'
+import Link from 'next/link'
+import { FC } from 'react'
+import Slider from 'react-slick'
 
-import LinkBtn from '@/components/LinkBtn/LinkBtn';
+import LinkBtn from '@/components/LinkBtn/LinkBtn'
 
-import styles from './Home.module.scss';
-import ImagesSlider from '@/components/ImagesSlider/ImagesSlider';
+import styles from './Home.module.scss'
+import ImagesSlider from '@/components/ImagesSlider/ImagesSlider'
 
 const Home: FC = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
-  return (
-    <div className={styles.wrapper}>
-      <section className={styles.media_container}>
-        <video
-          src="/video/main_full.mp4"
-          autoPlay
-          muted
-          loop
-          className={styles.main_full}
-          width="100%"
-          height="100%"
-        />
-        <video
-          src="/video/main_square (online-video-cutter.com).mp4"
-          autoPlay
-          muted
-          loop
-          className={styles.main_square}
-          width="100%"
-        />
-        <div className={styles.info}>
-          <h2>Best prices for mining equipment in the USA</h2>
-          <p>
-            Many years of contracts with the leading manufacturers of mining equipment allow us to
-            give you the most favorable conditions. Our own data center, more than 500,000 devices
-            brought in, more than 100,000 satisfied customers.
-          </p>
-          <div className={styles.links}>
-            <Link href="#">ACIS miners</Link>
-            <Link href="#">Data Center</Link>
-            <Link href="#">Containers</Link>
-            <Link href="#">Turnkey Mining</Link>
-          </div>
-        </div>
-      </section>
+	const settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+	}
+	return (
+		<div className={styles.wrapper}>
+			<section className={styles.media_container}>
+				<video
+					src="/video/main_full.mp4"
+					autoPlay
+					muted
+					loop
+					className={styles.main_full}
+					width="100%"
+					height="100%"
+				/>
+				<video
+					src="/video/main_square (online-video-cutter.com).mp4"
+					autoPlay
+					muted
+					loop
+					className={styles.main_square}
+					width="100%"
+				/>
+				<div className={styles.info}>
+					<h2>Best prices for mining equipment in the USA</h2>
+					<p>
+						Many years of contracts with the leading manufacturers of mining
+						equipment allow us to give you the most favorable conditions. Our
+						own data center, more than 500,000 devices brought in, more than
+						100,000 satisfied customers.
+					</p>
+					<div className={styles.links}>
+						<Link href="#">ACIS miners</Link>
+						<Link href="#">Data Center</Link>
+						<Link href="#">Containers</Link>
+						<Link href="#">Turnkey Mining</Link>
+					</div>
+				</div>
+			</section>
 
-      <section className={styles.benefis_wrapper}>
-        <div>
-          <Image
-            src="/image/index/Union1.svg"
-            alt=""
-            className="main__benefits-image"
-            width={40}
-            height={40}
-          />
-          <div className={styles.benefis_info}>
-            <h3 className="main__benefits-heading">Quality control</h3>
-            <p className="main__benefits-description">
-              We conduct a pre-sale inspection of all goods.
-            </p>
-          </div>
-        </div>
-        <div>
-          <Image
-            src="/image/index/Union2.svg"
-            alt=""
-            className="main__benefits-image"
-            width={40}
-            height={40}
-          />
-          <div className={styles.benefis_info}>
-            <h3 className="main__benefits-heading">24/7 Support</h3>
-            <p className="main__benefits-description">
-              We help you set up the equipment, provide extended warranty and quality service.
-            </p>
-          </div>
-        </div>
-        <div>
-          <Image
-            src="/image/index/Union4.svg"
-            alt=""
-            className="main__benefits-image"
-            width={40}
-            height={40}
-          />
-          <div className={styles.benefis_info}>
-            <h3 className="main__benefits-heading">Turnkey Mining</h3>
-            <p className="main__benefits-description">
-              Full support - from the purchase of equipment to connection to the pool and placement
-              in their own data center.
-            </p>
-          </div>
-        </div>
-        <div>
-          <Image
-            src="/image/index/Union.svg"
-            alt=""
-            className="main__benefits-img"
-            width={40}
-            height={40}
-          />
-          <div className={styles.benefis_info}>
-            <h3 className="main__benefits-heading">Direct supplies</h3>
-            <p className="main__benefits-description">
-              We work with manufacturers without intermediaries. Only original products.
-            </p>
-          </div>
-        </div>
-        <div>
-          <Image
-            src="/image/index/Union3.svg"
-            alt=""
-            className="main__benefits-img"
-            width={40}
-            height={40}
-          />
-          <div className={styles.benefis_info}>
-            <h3 className="main__benefits-heading">Fast shipping</h3>
-            <p className="main__benefits-description main__benefits-description-last">
-              We work only with reliable logistics companies.
-            </p>
-          </div>
-        </div>
-      </section>
+			<section className={styles.benefis_wrapper}>
+				<div>
+					<Image
+						src="/image/index/Union1.svg"
+						alt=""
+						className="main__benefits-image"
+						width={40}
+						height={40}
+					/>
+					<div className={styles.benefis_info}>
+						<h3 className="main__benefits-heading">Quality control</h3>
+						<p className="main__benefits-description">
+							We conduct a pre-sale inspection of all goods.
+						</p>
+					</div>
+				</div>
+				<div>
+					<Image
+						src="/image/index/Union2.svg"
+						alt=""
+						className="main__benefits-image"
+						width={40}
+						height={40}
+					/>
+					<div className={styles.benefis_info}>
+						<h3 className="main__benefits-heading">24/7 Support</h3>
+						<p className="main__benefits-description">
+							We help you set up the equipment, provide extended warranty and
+							quality service.
+						</p>
+					</div>
+				</div>
+				<div>
+					<Image
+						src="/image/index/Union4.svg"
+						alt=""
+						className="main__benefits-image"
+						width={40}
+						height={40}
+					/>
+					<div className={styles.benefis_info}>
+						<h3 className="main__benefits-heading">Turnkey Mining</h3>
+						<p className="main__benefits-description">
+							Full support - from the purchase of equipment to connection to the
+							pool and placement in their own data center.
+						</p>
+					</div>
+				</div>
+				<div>
+					<Image
+						src="/image/index/Union.svg"
+						alt=""
+						className="main__benefits-img"
+						width={40}
+						height={40}
+					/>
+					<div className={styles.benefis_info}>
+						<h3 className="main__benefits-heading">Direct supplies</h3>
+						<p className="main__benefits-description">
+							We work with manufacturers without intermediaries. Only original
+							products.
+						</p>
+					</div>
+				</div>
+				<div>
+					<Image
+						src="/image/index/Union3.svg"
+						alt=""
+						className="main__benefits-img"
+						width={40}
+						height={40}
+					/>
+					<div className={styles.benefis_info}>
+						<h3 className="main__benefits-heading">Fast shipping</h3>
+						<p className="main__benefits-description main__benefits-description-last">
+							We work only with reliable logistics companies.
+						</p>
+					</div>
+				</div>
+			</section>
 
-      <section className={styles.company_wrapper}>
-        <div className={styles.company_info}>
-          <h2>About company</h2>
-          <p>
-            The company's work is based on constant development in the field of blockchain and
-            cryptography. We bring modern technology into this world. That is why we make the field
-            of mining available to everyone.
-          </p>
-          <div className={styles.company_achievement}>
-            <div>
-              <h3 className="company__left-heading">7 years</h3>
-              <p className="company__left-description">
-                The company occupies a leading position in the international market
-              </p>
-            </div>
-            <div>
-              <h3 className="company__left-heading">100 Experts</h3>
-              <p className="company__left-description">
-                Experts make Promminer a leader in cryptocurrency mining
-              </p>
-            </div>
-            <div>
-              <h3 className="company__left-heading">100,000 units</h3>
-              <p className="company__left-description">ASIC miners delivered to our customers</p>
-            </div>
-            <div>
-              <h3 className="company__left-heading">40 000 000 $</h3>
-              <p className="company__left-description">Earned by our customers in 2022</p>
-            </div>
-          </div>
-          <LinkBtn title="Learn more" />
-        </div>
-        <div>{/* <ImagesSlider></ImagesSlider> */}</div>
-      </section>
-    </div>
-  );
-};
+			<section className={styles.company_wrapper}>
+				<div className={styles.company_info}>
+					<h2>About company</h2>
+					<p>
+						The company's work is based on constant development in the field of
+						blockchain and cryptography. We bring modern technology into this
+						world. That is why we make the field of mining available to
+						everyone.
+					</p>
+					<div className={styles.company_achievement}>
+						<div>
+							<h3 className="company__left-heading">7 years</h3>
+							<p className="company__left-description">
+								The company occupies a leading position in the international
+								market
+							</p>
+						</div>
+						<div>
+							<h3 className="company__left-heading">100 Experts</h3>
+							<p className="company__left-description">
+								Experts make Promminer a leader in cryptocurrency mining
+							</p>
+						</div>
+						<div>
+							<h3 className="company__left-heading">100,000 units</h3>
+							<p className="company__left-description">
+								ASIC miners delivered to our customers
+							</p>
+						</div>
+						<div>
+							<h3 className="company__left-heading">40 000 000 $</h3>
+							<p className="company__left-description">
+								Earned by our customers in 2022
+							</p>
+						</div>
+					</div>
+					<LinkBtn title="Learn more" />
+				</div>
+				<div>{/* <ImagesSlider></ImagesSlider> */}</div>
+			</section>
+		</div>
+	)
+}
 
-export default Home;
+export default Home
