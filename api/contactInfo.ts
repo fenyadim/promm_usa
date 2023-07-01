92b2d5207bb0c78e5b56f7daa841f86e215b4718
Dmitry <dimos9813@gmail.com>
2023-07-01 14:41:56 +0800

    add contact page

--- api/contactInfo.ts
+++ api/contactInfo.ts
@@ -9,9 +9,6 @@ type ContactType = {
 		mail?: string
 		phone?: string
 	}
-	options?: {
-		selectOnClick: boolean
-	}
 	coords?: PlacemarkGeometry | number[]
 }
 
@@ -25,7 +22,6 @@ export const contactInfo: Array<ContactType> = [
 			mail: 'info@promminer.ru',
 			phone: '8 (800) 505-57-22',
 		},
-		options: { selectOnClick: false },
 		coords: [55.824754, 37.415963],
 	},
 	{
@@ -36,9 +32,6 @@ export const contactInfo: Array<ContactType> = [
 			address: 'Irkutsk, Gorky str. 14B',
 			mail: 'irkutsk@promminer.ru',
 			phone: '8 (395) 248-23-34',
-		},
-		options: {
-			selectOnClick: false,
 		},
 		coords: [52.2815, 104.279915],
 	},
@@ -51,9 +44,6 @@ export const contactInfo: Array<ContactType> = [
 			mail: 'uae.promminer@gmail.com',
 			phone: '+971 58 574 6282',
 		},
-		options: {
-			selectOnClick: false,
-		},
 		coords: [25.186358, 55.260619],
 	},
 	{
@@ -64,9 +54,6 @@ export const contactInfo: Array<ContactType> = [
 			address: 'St. Petersburg, Izmailovsky Prospekt 2B',
 			phone: '8 (800) 505-57-22',
 		},
-		options: {
-			selectOnClick: false,
-		},
 		coords: [59.918025, 30.30817],
 	},
 	{
@@ -75,9 +62,6 @@ export const contactInfo: Array<ContactType> = [
 			src: '/image/contacts/Hong.png',
 			title: 'Hong-Kong office',
 			address: '14 Science Museum Rd, Tsim Sha Tsui East, Гонконг',
-		},
-		options: {
-			selectOnClick: false,
 		},
 		coords: [22.29526, 114.174947],
 	},
