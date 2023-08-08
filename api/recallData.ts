e7115bd53ed5a5e415111a1557a05da11029834e
Jambeezy <106315773+Jambeezy@users.noreply.github.com>
2023-08-09 00:33:35 +0800

    Add files via upload

--- api/recallData.ts
+++ api/recallData.ts
@@ -2,15 +2,63 @@ import { RecallType } from '@/types/recall.type';
 
 export const recallData: RecallType[] = [
   {
-    src: "/image/people/Frame-102.jpg",
-    date: '18.02.2023',
-    name: 'Pavel Chehomov',
-    text: "I ordered from another city - it was delivered on time and even a littleearlier. The cables, documentation and other paperwork are in place. Atmy request, they recorded a video in advance and helped with a couple ofdifficulties that arose, I haven't found any disadvantages yet, but ifanything, I'll write.",
+    src: "/image/people/2.jpg",
+    date: '05.07.2023',
+    name: 'Simon D',
+    text: "Easy to deal with and us. Based I went with the priority shipping and had my order 2 days after I received the tracking and only 3 days total. Some places take a week to even provide tracking. I’ll be a return customer.",
   },
   {
-    src: "/image/people/Frame-102.jpg",
-    date: '18.02.2023',
-    name: 'Pavel Chehomov',
-    text: "I ordered from another city - it was delivered on time and even a littleearlier. The cables, documentation and other paperwork are in place. Atmy request, they recorded a video in advance and helped with a couple ofdifficulties that arose, I haven't found any disadvantages yet, but ifanything, I'll write.",
+    src: "/image/people/3.jpg",
+    date: '22.06.2023',
+    name: 'Jim B',
+    text: "Excellent Product. Exceeds expectations. Make sure you update to Firmware V.6786 which eliminates high stale rate and maintains 316-319mh/s average. Let's not forget about that professional packaging with the Styrofoam suitcase. Getting ready to buy a second one! Thank you Apollo!",
+  },
+  {
+    src: "/image/people/4.jpg",
+    date: '21.06.2023',
+    name: 'Colin K',
+    text: "I love the miner and customer service I would recommend promminer to anyone looking for good deals! My apollo V1 mini classic has been hashing on average at about 151 mhz which is great! Profitable and good for beginners!",
+  },
+  {
+    src: "/image/people/5.jpg",
+    date: '19.06.2023',
+    name: 'Richard S',
+    text: "Great buying experience, competitive prices, fast shipping, sturdy packaging, Thank you!",
+  },
+  {
+    src: "/image/people/6.jpg",
+    date: '18.06.2023',
+    name: 'William A',
+    text: "If this company I bought this model miner ipollo still now I am so satisfied and has low noise and Low power consumption , relay so fast shipping  with cheap price and cheap shipping 😱👌💸",
+  },
+  {
+    src: "/image/people/7.jpg",
+    date: '03.06.2023',
+    name: 'Lucas G',
+    text: "Very disappointed in Apollo not honoring full replacement warranty for 3 day old product, you don't repair brand new equipment! Thank you Leonid L. @Promminer for stepping in and honoring the warranty for them.  Belay Mining went above and beyond to satisfy a customer, if I need more mining equipment you can bet I'm coming here.  I'm glad to be back up and running eking out that last bit of EARTH before the merge. Thanks again for the great customer service..",
+  },
+  {
+    src: "/image/people/8.jpg",
+    date: '29.05.2023',
+    name: 'Calvin P',
+    text: "Had some issues setting it up and finding the IP but ended up getting the help i needed!It mines over the advertised amount at about 150mh/s Sometimes it can dip lower and sometimes even higher but generally stays in the lower 100's-150. I do wish it were a little more consistent because it has dipped under 100mh/s but fortunately it usually doesnt stay that low for long Earning around .04etc/day THANKS SO MUCH! VERY VERY QUIET. Compared to an S9 its not even perceptible. Great little device, just wish it had more VRAM",
+  },
+  {
+    src: "/image/people/9.jpg",
+    date: '25.05.2023',
+    name: 'David O',
+    text: "Second purchase from Prominent in past 2 months and as usual, great pricing, fast delivery and help when you need it. Bought the V1 Mini Gold just prior to the EARTH change to keep up with expected increase in Hashrate and it was most definitely warranted. Highly recommended source for your mining equipment!",
+  },
+  {
+    src: "/image/people/11.jpg",
+    date: '21.05.2023',
+    name: 'David O',
+    text: "Awesome service. I received my grin miner within 4 business days. I was up and mining in no time. Thank you, Promminer",
+  },
+  {
+    src: "/image/people/12.jpg",
+    date: '19.05.2023',
+    name: 'Scott S',
+    text: "I placed my order with Delay as theSE Plus model was USA stock and best price I could find. I received an email 1 day later with shipment info and it arrived 3 days after I placed my order. Hashing beautifully with 2 x SE models and the older apollo 'gold' model. Doing just over 400 mh/s @ 234 watts. Altogether 1100+ mh/s @ 702 watts. Profitable at 14 cent power. Will do more business with Promminer.",
   }
 ]
