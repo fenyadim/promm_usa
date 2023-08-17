566c0e76ccabf8cf4e63c5040a353c1372af60b1
Dmitry <dimos9813@gmail.com>
2023-08-17 20:14:00 +0800

    add google metrik

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Head from 'next/head'
+import Script from 'next/script'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import 'swiper/scss/navigation'
@@ -33,6 +34,18 @@ export default function RootLayout({
 						</PersistGate>
 					</Provider>
 				</Providers>
+				<Script
+					strategy="lazyOnload"
+					src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+				/>
+				<Script strategy="lazyOnload">
+					{`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          `}
+				</Script>
 			</body>
 		</html>
 	)
